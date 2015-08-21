174659b7e6be8360445ffdfa168dbcec8ef8f080
I826342 <jean-paul.robinson@sap.com>
2015-08-21 14:05:35 -0400

    Test with Node.rm and override variables

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 ### testbase attributes
-
+node.rm('test_attr', 'testvar') 
 override['test_attr']['controlvar']  = 'This value has been changed', ## Control group
 override['test_attr']['testvar'] = nil 	## Experimental group. 
 													
