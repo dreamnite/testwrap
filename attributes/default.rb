4341219894f40389cb5f4a02991a419358a0a16d
I826342 <jean-paul.robinson@sap.com>
2015-08-21 12:02:51 -0400

    Initial wrapper test - override variables

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+### testbase attributes
+
+override['test_attr']['control']  = 'This value has been changed', ## Control group
+override['test_attr']['test'] = nil 	## Experimental group. 
+													
