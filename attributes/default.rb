1e1b41c1bebd789966c7a0a6bcaabd11cba7c39b
I826342 <jean-paul.robinson@sap.com>
2015-08-21 14:42:39 -0400

    Noticed a typo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 ### testbase attributes
 
-override['test_attr']['controlvar']  = 'This value has been changed', ## Control group
+override['test_attr']['controlvar']  = 'This value has been changed' ## Control group
 override['test_attr']['testvar'] = nil 	## Experimental group. 
 													
